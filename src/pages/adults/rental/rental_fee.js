ae0d25b6ed3c8c8b7f9d1993b93dc1b108b79d82
atjjn <atsuki.0908@icloud.com>
2023-09-24 19:00:17 +0900

    レンタルスペースの料金記載

--- src/pages/adults/rental/rental_fee.js
+++ src/pages/adults/rental/rental_fee.js
@@ -1,11 +1,39 @@
 import Container from "@/components/container";
 import Meta from "@/components/meta";
+import style from "../../../styles/rental.module.css";
 export default function RentalFee() {
   return (
     <>
       <Container large>
         <Meta pageTitle="料金案内" />
-        レンタルスペースの料金案内
+        <h1>レンタルスペースの料金案内</h1>
+        <div className={style.backFrame}>
+          <div>
+            <h2>料金について</h2>
+            <table className={style.tableBordered}>
+              <thead>
+                <tr>
+                  <th>時間</th>
+                  <th>料金</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th>1時間</th>
+                  <th>1,000円</th>
+                </tr>
+                <tr>
+                  <th>1時間</th>
+                  <th>1,000円</th>
+                </tr>
+                <tr>
+                  <th>1時間</th>
+                  <th>1,000円</th>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
       </Container>
     </>
   );
