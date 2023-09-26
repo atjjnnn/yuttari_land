bb0ec97de7c458a0b299d41bdf011d9524a7b2b4
atjjn <atsuki.0908@icloud.com>
2023-09-27 08:22:57 +0900

    パス修正

--- src/pages/news/index.js
+++ src/pages/news/index.js
@@ -1,6 +1,6 @@
-import Meta from 'components/meta'
-import Container from 'components/container'
-import Hero from 'components/hero'
+import Meta from "src/components/meta";
+import Container from "src/components/container";
+import Hero from "src/components/hero";
 
 export default function Blog() {
   return (
@@ -8,5 +8,5 @@ export default function Blog() {
       <Meta pageTitle="ブログ" pageDesk="ブログの記事一覧" />
       <Hero title="Blog" subtitle="Recent Posts" />
     </Container>
-  )
+  );
 }
