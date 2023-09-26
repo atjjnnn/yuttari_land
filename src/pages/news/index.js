e8719517f6ddf683d1cac4177240846f34cc0521
atjjn <atsuki.0908@icloud.com>
2023-09-27 08:24:36 +0900

    不要なインポート削除

--- src/pages/news/index.js
+++ src/pages/news/index.js
@@ -1,12 +1,10 @@
 import Meta from "src/components/meta";
 import Container from "src/components/container";
-import Hero from "src/components/hero";
 
 export default function Blog() {
   return (
     <Container>
       <Meta pageTitle="ブログ" pageDesk="ブログの記事一覧" />
-      <Hero title="Blog" subtitle="Recent Posts" />
     </Container>
   );
 }
