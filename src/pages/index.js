6ee774a8bde5276d879952b7a92e9e47dc343b7d
atjjn <atsuki.0908@icloud.com>
2023-10-19 07:38:34 +0900

    スタイル修正

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,24 +26,22 @@ export default function Home() {
 
         <div className={home.ttl_center}>
           <div className={home.areaCurrent}>
-            <figure className={home.image}>
-              <Image src={currentClass} alt="" />
-            </figure>
-            <br />
             <div>
               <p>
                 ”色彩知育 カレントクラス”では、最新の脳科学をベースにした
-                <br />
                 日本こども色彩協会オリジナルメソッドである新しい教育『色彩知育法』を取り入れ、
-                <br />
                 こどもが自分で答えを出し、自らの人生をつくっていけるような環境を提供します。
-                <br />
                 また、親が”日常で出来ること”を学べる機会を作り、親も子も共に学べる環境づくりを目指しています。
                 <br />
               </p>
-              <Link href="https://kodomo-shikisai.com/" className={home.button}>
-                日本こども色彩協会とは
-              </Link>
+              <div className={home.buttonDiv}>
+                <Link
+                  href="https://kodomo-shikisai.com/"
+                  className={home.button}
+                >
+                  日本こども色彩協会とは
+                </Link>
+              </div>
             </div>
           </div>
         </div>
