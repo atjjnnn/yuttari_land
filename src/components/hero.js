88bb90f7ccee15b3575b193c5d723f4b2e8a839f
atjjn <atsuki.0908@icloud.com>
2023-10-19 08:26:25 +0900

    スタイル調整

--- src/components/hero.js
+++ src/components/hero.js
@@ -3,10 +3,7 @@ import styles from "src/styles/hero.module.css";
 export default function Hero() {
   return (
     <section className={styles.hero}>
-      <div className={styles.title}>
-        <h1>ゆったりランド</h1>
-        <p>ゆったりできる場所</p>
-      </div>
+      <div className={styles.title}></div>
     </section>
   );
 }
